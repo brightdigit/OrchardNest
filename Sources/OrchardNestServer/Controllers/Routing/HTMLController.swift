63a441ed1f448f889743a80a62796e9bb0793ac7
Leo Dion <leogdion@brightdigit.com>
2020-08-03 15:50:19 -0400

    adding first version of html

--- Sources/OrchardNestServer/Controllers/Routing/HTMLController.swift
+++ Sources/OrchardNestServer/Controllers/Routing/HTMLController.swift
@@ -6,6 +6,41 @@ import Vapor
 
 struct InvalidDatabaseError: Error {}
 
+extension String {
+  var plainTextShort: String {
+    String(replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil).prefix(240))
+  }
+}
+
+extension EntryCategoryType {
+  var elClass: String {
+    switch self {
+    case .companies:
+      return "website"
+    case .design:
+      return "brush"
+    case .development:
+      return "cogs"
+    case .marketing:
+      return "bullhorn"
+    case .newsletters:
+      return "envelope"
+    case .podcasts:
+      return "podcast"
+    case .updates:
+      return "file-new"
+    case .youtube:
+      return "video"
+    }
+  }
+}
+
+extension EntryCategory {
+  var elClass: String {
+    return type.elClass
+  }
+}
+
 struct HTMLController {
   func index(req: Request) -> EventLoopFuture<HTML> {
     /*
@@ -17,6 +52,10 @@ struct HTMLController {
 
     return Entry.query(on: req.db).join(LatestEntry.self, on: \Entry.$id == \LatestEntry.$id).with(\.$channel)
       .join(parent: \.$channel)
+      .with(\.$podcastEpisodes)
+      .join(children: \.$podcastEpisodes, method: .left)
+      .with(\.$youtubeVideos)
+      .join(children: \.$youtubeVideos, method: .left)
       .filter(Channel.self, \Channel.$category.$id != "updates")
       .sort(\.$publishedAt, .descending)
       .limit(32)
@@ -27,8 +66,19 @@ struct HTMLController {
       .map { (page) -> HTML in
         HTML(
           .head(
-            .title("OrchardNest"),
-            .link(.rel(.stylesheet), .href("/styles/milligram.css"))
+            .title("OrchardNest - Swift Articles and News"),
+//            <link rel="stylesheet" type="text/css" href="/styles/elusive-icons/css/elusive-icons.min.css"/>
+//                <link rel="stylesheet" type="text/css" href="/styles/normalize.css"/>
+//                <link rel="stylesheet" type="text/css" href="/styles/milligram.css"/>
+//                <link rel="stylesheet" type="text/css" href="/styles/style.css">
+//                <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;400;800&display=swap" rel="stylesheet">
+            .link(.rel(.stylesheet), .href("/styles/elusive-icons/css/elusive-icons.min.css")),
+
+            .link(.rel(.stylesheet), .href("/styles/normalize.css")),
+
+            .link(.rel(.stylesheet), .href("/styles/milligram.css")),
+            .link(.rel(.stylesheet), .href("/styles/style.css")),
+            .link(.rel(.stylesheet), .href("https://fonts.googleapis.com/css2?family=Catamaran:wght@100;400;800&display=swap"))
           ),
           .body(
             .div(
@@ -38,21 +88,77 @@ struct HTMLController {
                 .div(
                   .class("column"),
                   .h1("OrchardNest"),
-                  .p("Writing HTML in Swift is pretty great!"),
+                  .p("Swift Articles and News"),
 
-                  .ul(.forEach(page) {
-                    .li(
-                      .class("blog-post"),
+                  .ul(
+                    .class("articles"),
+                    .forEach(page) {
+                      /*
+                       <li class="blog-post">
+                                     <a class="title" href="#"><h3><i class="el el-website"></i> Swift: Property wrappers</h3></a>
+                                     <div class="publishedAt">Published July 31, 2020</div>
+                                     <div class="summary">
+                                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet.
+                                     </div>
+                                     <div class="author">By John Doe at <a href="/sites/">swiftui.com</a></div>
+                                   </li>
+                       */
+                      .li(
+                        .class("blog-post"),
 
-                      .a(
-                        .href($0.url),
-                        .div(
+                        .a(
+                          .href($0.url),
                           .class("title"),
-                          .text($0.title)
+                          .h3(
+                            .i(.class("el el-\($0.category.elClass)")),
+
+                            .text($0.title)
+                          )
+                        ),
+                        .div(
+                          .class("publishedAt"),
+                          .text($0.publishedAt.description)
+                        ),
+                        .unwrap($0.youtubeID) {
+                          .iframe(
+                            .src("https://www.youtube.com/embed/" + $0)
+                          )
+//                        <iframe width="560" height="315" src="https://www.youtube.com/embed/GCQ2JtEuGsI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        },
+                        .div(
+                          .class("summary"),
+                          .text($0.summary.plainTextShort)
+                        ),
+                        .unwrap($0.podcastEpisodeURL) {
+                          .audio(
+                            .controls(true),
+                            .source(
+                              .src($0)
+                            )
+                          )
+//                        <audio controls>
+//                         <source src="http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4"
+//                                 type='audio/mp4'>
+//                         <!-- The next two lines are only executed if the browser doesn't support MP4 files -->
+//                         <source src="http://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.oga"
+//                                 type='audio/ogg; codecs=vorbis'>
+//                         <!-- The next line will only be executed if the browser doesn't support the <audio> tag-->
+//                         <p>Your user agent does not support the HTML5 Audio element.</p>
+//                        </audio>
+                        },
+                        .div(
+                          .class("author"),
+                          .text("By "),
+                          .text($0.channel.author),
+                          .text(" at "),
+                          .a(
+                            .href("/channels/" + $0.channel.id.uuidString),
+                            .text($0.channel.siteURL.host ?? $0.channel.title)
+                          )
                         )
                       )
-                    )
-                  })
+                    }
+                  )
                 )
               )
             )
