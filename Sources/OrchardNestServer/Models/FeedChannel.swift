49fcd418364d034a740745baf952116e6620e7db
Leo Dion <leogdion@brightdigit.com>
2020-08-08 16:05:40 -0400

    fixing code for ga

--- Sources/OrchardNestServer/Models/FeedChannel.swift
+++ Sources/OrchardNestServer/Models/FeedChannel.swift
@@ -7,27 +7,26 @@ import Vapor
 
 struct EmptyError: Error {}
 
-
 extension FeedChannel {
   static func parseSite(_ site: OrganizedSite, using client: Client, on eventLoop: EventLoop) -> EventLoopFuture<Result<FeedChannel, FeedError>> {
     // let uri = URI(string: site.site.feed_url.absoluteString)
     // let headers = HTTPHeaders([("Host", uri.host!), ("User-Agent", "OrchardNest-Robot"), ("Accept", "*/*")])
 
-    //let promise = eventLoop.makePromise(of: Result<Data, Error>.self)
+    // let promise = eventLoop.makePromise(of: Result<Data, Error>.self)
     return client.get(URI(string: site.site.feed_url.absoluteString)).map { (response) -> Data? in
-      response.body.map { (buffer) in
+      response.body.map { buffer in
         Data(buffer: buffer)
       }
     }.flatMapAlways { (result) -> EventLoopFuture<Result<FeedChannel, FeedError>> in
-      let newResult = result.mapError{FeedError.download(site.site.feed_url, $0)}.flatMap { (data) -> Result<Data, FeedError> in
+      let newResult = result.mapError { FeedError.download(site.site.feed_url, $0) }.flatMap { (data) -> Result<Data, FeedError> in
         guard let data = data else {
           return .failure(.empty(site.site.feed_url))
         }
         return .success(data)
-      }.flatMap { (data) in
-        return Result{
+      }.flatMap { data in
+        Result {
           try FeedChannel(language: site.languageCode, category: site.categorySlug, site: site.site, data: data)
-        }.mapError{.parser(site.site.feed_url, $0)}
+        }.mapError { .parser(site.site.feed_url, $0) }
       }.flatMap { (channel) -> Result<FeedChannel, FeedError> in
         guard channel.items.count > 0 || channel.itemCount == channel.items.count else {
           return .failure(.items(site.site.feed_url))
